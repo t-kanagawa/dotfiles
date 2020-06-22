05532028c344c96edc9f92be3264ac70f9ff76ca
takanaro <takashi.kngw@gmail.com>
2020-06-23 00:25:15 +0900

    fix

--- .bin/application/apt.sh
+++ .bin/application/apt.sh
@@ -5,14 +5,15 @@ set -ue
 # Docker Composeインストール
 #
 
-function main () {
-  sudo apt update \
-  && sudo apt upgrade -y \
-  && sudo apt install -y \
-  jq \ # json変換
-  vim \ # Vim
-  cron # 定期実行
+function main() {
+  sudo apt update &&
+    sudo apt upgrade -y &&
+    sudo apt install -y \
+      jq \
+      vim \
+      cron #定期実行
 
+  # cronサービス起動
   sudo systemctl enable cron && sudo systemctl restart cron
 }
 
