a93eb73720762e240fe34f905cdf3131b8c6b51e
takanaro <takashi.kngw@gmail.com>
2021-02-12 00:13:27 +0900

    Google SpreadSheetのパッケージなどをインストールするように変更

--- .bin/application/selenium.sh
+++ .bin/application/selenium.sh
@@ -15,6 +15,7 @@ apt update && apt install -y google-chrome-stable python3-pip
 chrome_version=$(google-chrome --version | perl -pe 's/([^0-9]+)([0-9]+\.[0-9]+).+/$2/g')
 
 pip3 install selenium chromedriver-binary~=${chrome_version}
+pip3 install gspread oauth2client
 
 # chrome上でスクリーンショットを利用するために必要
 apt install -y fonts-ipafont-gothic --no-install-recommends
--- .bin/init.sh
+++ .bin/init.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/env bash -eu
+#!/bin/bash -eu
 
-if [ "$(whoami)" != "root" ]; then
-  echo "Require root privilege"
-  exit 1
-fi
+# if [ "$(whoami)" != "root" ]; then
+#   echo "Require root privilege"
+#   exit 1
+# fi
 
 timedatectl set-timezone Asia/Tokyo
 localectl set-locale LANG=ja_JP.utf8
 localectl set-keymap jp106
+git config --global core.editor 'vim -c "set fenc=utf-8"'
+
+echo "complete"
